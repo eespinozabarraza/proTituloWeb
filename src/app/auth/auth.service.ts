f6d2b8f38a7f37c3419f4dbd484d7a99fb007b0e
neomirc <neomirc@msn.com>
2019-09-18 12:23:58 -0300

    Se reordena el header

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Router } from  "@angular/router";
 import { auth } from  'firebase/app';
 import { AngularFireAuth } from  "@angular/fire/auth";
 import { User } from  'firebase';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
